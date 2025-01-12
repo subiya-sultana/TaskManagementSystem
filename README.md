40b9a47b57b8053437799e50036e755fe02045e3
subiya-sultana <itsmesubiya@gmail.com>
2025-01-12 17:47:11 +0530

    changes

--- README.md
+++ README.md
@@ -1,7 +1,57 @@
-hellooo....
-here is the quick guide on how to run this project in your localhost
-step 1: make sure u have XAMPP installed in ur system
-step 2: go to Xampp , open htdocs then there clone this repo using git clone "https://github.com/subiya-sultana/TaskManagementSystem.git"
-step 3: now Ensure that Apache and MySQL are running in the XAMPP Control Panel.
-sep 4: add database, Open phpmyadmin at http://localhost/phpmyadmin, create database with name "taskmanagementsystem", Click Import and import the "taskmanagementsystem.sql" file from database directory provided in this repo
-step 5: go to http://localhost/TaskManagementSystem or directly on http://localhost/TaskManagementSystem/homepage.php
+
+# Task Management System
+
+---
+
+## How to Set Up and Run the Project on Localhost
+
+Follow these steps to set up the project on your local machine:
+
+### **Step 1: Install XAMPP**  
+Ensure you have [XAMPP](https://www.apachefriends.org/index.html) installed on your system. XAMPP is essential to run the PHP server and MySQL database.
+
+---
+
+### **Step 2: Clone the Repository**  
+1. Open the `htdocs` directory in your XAMPP installation folder.  
+   - For example, `C:\xampp\htdocs`.  
+2. Clone this repository into the `htdocs` folder using the following command:  
+   ```bash
+   git clone https://github.com/subiya-sultana/TaskManagementSystem.git
+   ```
+
+---
+
+### **Step 3: Start Apache and MySQL**  
+1. Open the XAMPP Control Panel.  
+2. Start the **Apache** and **MySQL** modules.
+
+---
+
+### **Step 4: Set Up the Database**  
+1. Open [phpMyAdmin](http://localhost/phpmyadmin) in your browser.  
+2. Create a new database named **`taskmanagementsystem`**.  
+3. Import the SQL file:  
+   - Go to the **Import** tab.  
+   - Select the `taskmanagementsystem.sql` file from the `database` directory in this repository.  
+   - Click **Go** to execute the import.
+
+---
+
+### **Step 5: Access the Project**  
+1. Open your browser and go to:  
+   - [http://localhost/TaskManagementSystem](http://localhost/TaskManagementSystem)  
+   - Or directly access the homepage: [http://localhost/TaskManagementSystem/homepage.php](http://localhost/TaskManagementSystem/homepage.php).  
+2. You should now see the Task Management System running on your localhost.
+
+---
+
+### **Step 6: Demo Account Details**  
+To explore the project as an admin, use the following demo account credentials:  
+- **Email**: admin@gmail.com  
+- **Username**: admin  
+- **Password**: admin
+
+---
+
+Feel free to test and explore the features of the project. Let me know if you encounter any issues!
--- database/taskmanagementsystem.sql
+++ database/taskmanagementsystem.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Nov 25, 2022 at 04:00 PM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Jan 12, 2025 at 01:15 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -18,7 +18,7 @@ SET time_zone = "+00:00";
 /*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `subiya`
+-- Database: `taskmanagementsystem`
 --
 
 -- --------------------------------------------------------
@@ -35,25 +35,21 @@ CREATE TABLE `tasks` (
   `priority` int(10) DEFAULT 4,
   `timestamp` timestamp NOT NULL DEFAULT current_timestamp(),
   `user-id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `tasks`
 --
 
 INSERT INTO `tasks` (`Sno`, `task-name`, `task-desc`, `due-date`, `priority`, `timestamp`, `user-id`) VALUES
-(1, 'is it working now??', 'i think yessss', '2022-11-25 18:29:00', 4, '2022-11-25 14:51:33', 1),
-(2, 'hello this is today task with 1st priority', 'and this is first priority task description', '2022-11-25 18:29:00', 1, '2022-11-25 14:52:03', 1),
-(3, '2nd priority task', '', '2022-11-25 18:29:00', 2, '2022-11-25 14:52:20', 1),
-(4, 'another task with 1st priority here', 'and also with due date of 1 jan 2023', '2023-01-01 18:29:00', 1, '2022-11-25 14:53:14', 1),
-(5, 'hii this is task with 3rd priority', '', '2022-12-11 18:29:00', 3, '2022-11-25 14:53:46', 1),
-(6, 'task with no priorityy sett', '', '2022-11-25 18:29:00', 4, '2022-11-25 14:54:21', 1),
-(7, 'aaaa', 'aaaa', '2022-11-25 18:29:00', 4, '2022-11-25 14:54:43', 1),
-(8, 'zzzz', 'zzzz', '2022-11-25 18:29:00', 4, '2022-11-25 14:54:57', 1),
-(9, 'another task here ', '', '2022-11-25 18:29:00', 2, '2022-11-25 14:55:21', 1),
-(10, 'my task', '', '2022-11-25 18:29:00', 2, '2022-11-25 14:55:30', 1),
-(11, 'why it is not getting save in database?', 'now it got saved ', '2022-11-25 18:29:00', 3, '2022-11-25 14:56:04', 1),
-(12, 'completeddddddddd', 'yeahhhhhhhh', '2022-11-25 18:29:00', 4, '2022-11-25 14:56:18', 1);
+(1, 'Complete the Project Report', 'prepare PPT, write project report, add screenshots of the website', '2025-01-12 18:29:00', 4, '2025-01-12 11:59:09', 1),
+(2, 'Host OrganizeU on Render', 'host this application on render', '2025-01-12 18:29:00', 1, '2025-01-12 11:59:53', 1),
+(3, 'Bug Fixes in the ecommerce website', 'The bugs are still crawling around in my system. I swear I will fix them, but my procrastination is winning right now. 😬', '2025-01-12 18:29:00', 3, '2025-01-12 12:00:56', 1),
+(4, 'Prepare for finals', 'Stop studying one day before exam, and start actual studies', '2025-02-20 18:29:00', 1, '2025-01-12 12:03:57', 1),
+(5, 'Code Review Session', 'Got to go through the code for that new feature.', '2025-02-13 18:29:00', 2, '2025-01-12 12:05:13', 1),
+(6, 'Project Presentation Preparation', 'Time to prepare slides and rehearse the presentation.', '2025-01-28 18:29:00', 2, '2025-01-12 12:06:30', 1),
+(7, 'Start Learning React Native', ' I need to get into mobile app development. Hopefully, this will stop me from coding for hours without building anything. 📱💻', '2025-03-13 18:29:00', 3, '2025-01-12 12:07:54', 1),
+(8, 'Register for eduSkills intership', 'Login and register for cohort 11', '2025-01-13 18:29:00', 4, '2025-01-12 12:09:31', 1);
 
 -- --------------------------------------------------------
 
@@ -67,14 +63,14 @@ CREATE TABLE `users` (
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `email`, `username`, `password`, `created_at`) VALUES
-(1, 'itsmesubiya@gmail.com', 'subiya', '$2y$10$Wyk6Wg15TGUGd.9DDtYgbOlziJdO/9O94t9K5SBR.yNVNcQdzwwz2', '2022-11-25 20:20:28');
+(1, 'admin@gmail.com', 'admin', '$2y$10$BGNMIJ.AClGYKWGHgetJpOeqz7tyean1V9b9bS23RH1HVesywxUf2', '2025-01-12 17:27:31');
 
 --
 -- Indexes for dumped tables
@@ -101,13 +97,13 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `Sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=13;
+  MODIFY `Sno` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=23;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
 -- Constraints for dumped tables
